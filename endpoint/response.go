0689012a86752a1d1d7b274046954056fca588d7
Yonathan Setiadi <setiadi_y56@yahoo.co.id>
2021-03-17 15:12:49 +0700

    #6 create endpoint and transport

--- endpoint/response.go
+++ endpoint/response.go
@@ -1,8 +1,28 @@
 package endpoint
 
-import "github.com/sapawarga/video-service/model"
+import (
+	"time"
+
+	"github.com/sapawarga/video-service/model"
+)
 
 type VideoResponse struct {
 	Data     []*model.VideoResponse `json:"data"`
 	Metadata *model.Metadata        `json:"metadata"`
+}
+
+type VideoDetail struct {
+	ID           int64
+	Title        string
+	CategoryID   *int64
+	CategoryName *string
+	Source       string
+	VideoURL     string
+	RegencyID    *int64
+	RegencyName  *string
+	Status       int64
+	CreatedAt    *time.Time
+	UpdatedAt    *time.Time
+	CreatedBy    *int64
+	UpdatedBy    *int64
 }
--- go.mod
+++ go.mod
@@ -7,5 +7,5 @@ require (
 	github.com/golang/mock v1.5.0
 	github.com/onsi/ginkgo v1.7.0
 	github.com/onsi/gomega v1.4.3
-	github.com/sapawarga/proto-file v0.0.0-20210317062517-0538660789a6
+	github.com/sapawarga/proto-file v0.0.0-20210317075032-bce22a7dd375
 )
